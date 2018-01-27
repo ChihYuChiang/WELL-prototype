a9375a6772780570250817ba22b90c3e05c1ef80
ChihYuChiang <ckccyu@gmail.com>
2018-01-27 08:19:10 -0800

    Add: rotation loop

--- WellPt/MainWindow.xaml.cs
+++ WellPt/MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace WellPt
         public MainWindow()
         {
             InitializeComponent();
+
+            Storyboard sb = (this.FindResource("sbAnimateImage2") as Storyboard);
+            sb.Begin();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
