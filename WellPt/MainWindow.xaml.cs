52ec51b3d1cbaa102d8da0bfc33c467d3ccbb8b0
ChihYuChiang <ckccyu@gmail.com>
2018-01-29 15:02:49 -0800

    Add: question binding

--- WellPt/MainWindow.xaml.cs
+++ WellPt/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 using Microsoft.VisualBasic.FileIO;
 using System.IO;
 
@@ -20,12 +21,19 @@ namespace WellPt
 {
     public partial class MainWindow : Window
     {
+        public static List<string[]> qBook = ParseCSV(new StringReader(Properties.Resources.QBook));
+
+        private QItem dItem = new QItem() { Prompt = MainWindow.qBook[1][1] };
+
         public MainWindow()
         {
             InitializeComponent();
 
             Storyboard sb = (this.FindResource("sbAnimateImage2") as Storyboard);
             sb.Begin();
+
+            Binding myBinding = new Binding("Prompt") { Source = dItem, Mode = BindingMode.OneWay };
+            BindingOperations.SetBinding(ui_dItem_prompt, Label.ContentProperty, myBinding);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -33,11 +41,10 @@ namespace WellPt
             Storyboard sb = (this.FindResource("sbAnimateImage") as Storyboard);
             sb.Begin();
 
-            StringReader csvReader = new StringReader(Properties.Resources.QBook);     
-            List<string[]> qBook = ParseCSV(csvReader);
+            dItem.Prompt = "test us to do so";
         }
 
-        public List<string[]> ParseCSV(StringReader text)
+        public static List<string[]> ParseCSV(StringReader text)
         {
             string[] fields;
             List<string[]> parsedData = new List<string[]>();
@@ -57,6 +64,34 @@ namespace WellPt
             string line = parser.ReadLine();
 
             return parsedData;
+        }
+    }
+
+    public class QItem : INotifyPropertyChanged
+    {
+        private string prompt;
+        
+        public int Id { get; set; }
+        public int Ans { get; set; }
+        public string[] Opts { get; set; }
+        public string Prompt
+        {
+            get { return this.prompt; }
+            set
+            {
+                if (this.prompt != value)
+                    {
+                        this.prompt = value;
+                        this.NotifyPropertyChanged("Prompt");
+                    }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
         }
     }
 }
